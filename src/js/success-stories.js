3b54eec08c729eeba833548e44f299958edb26a5
zeven <frostdi40@gmail.com>
2026-06-17 00:47:41 +0300

    update

---
+++ src/js/success-stories.js
@@ -0,0 +1,167 @@
+import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiperFeedback = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  allowTouchMove: false,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: false,
+  },
+
+  navigation: {
+    nextEl: '.arrow-button-forward',
+    prevEl: '.arrow-button-back',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
+swiperFeedback.on('reachEnd', () => {
+  console.log('end');
+});
+let PER_PAGE = 3;
+let TOTAL;
+let TOTAL_PAGES;
+let CURRENT_PAGE;
+const refs = {
+  buttonNext: document.querySelector('.js-button-back'),
+  buttonPrev: document.querySelector('.js-button-forward'),
+};
+
+function slideTemplate(elem) {
+  return `        <div class="swiper-slide">
+          <ul class="feedback-list">
+            <li class="feedback-item">
+              <div class="rating value-${Math.ceil(elem.rate)} star-svg">
+                <div class="star-container" id="stars-wrapper">
+                  <div class="star">
+                    <svg class="star-empty" id="empty-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-empty"
+                      ></use>
+                    </svg>
+                    <svg class="star-filled" id="filled-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-filled"
+                      ></use>
+                    </svg>
+                  </div>
+                  <div class="star">
+                    <svg class="star-empty" id="empty-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-empty"
+                      ></use>
+                    </svg>
+                    <svg class="star-filled" id="filled-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-filled"
+                      ></use>
+                    </svg>
+                  </div>
+                  <div class="star">
+                    <svg class="star-empty" id="empty-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-empty"
+                      ></use>
+                    </svg>
+                    <svg class="star-filled" id="filled-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-filled"
+                      ></use>
+                    </svg>
+                  </div>
+                  <div class="star">
+                    <svg class="star-empty" id="empty-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-empty"
+                      ></use>
+                    </svg>
+                    <svg class="star-filled" id="filled-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-filled"
+                      ></use>
+                    </svg>
+                  </div>
+                  <div class="star">
+                    <svg class="star-empty" id="empty-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-empty"
+                      ></use>
+                    </svg>
+                    <svg class="star-filled" id="filled-black-star">
+                      <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="../img/sprite.svg#icon-star-filled"
+                      ></use>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <p class="feedback-comment">${elem.description}</p>
+              <p class="feedback-author">${elem.author}</p>
+            </li>
+          </ul>
+        </div>`;
+}
+
+function createSlides(array) {
+  const markup = array.map(el => slideTemplate(el));
+  renderSlides(markup)
+}
+function renderSlides(slides) {
+  swiperFeedback.appendSlide([...slides]);
+}
+
+createSlides([
+  {
+    _id: '667abea5e4b0c8a2a7c1c1fa',
+    description:
+      'Ми взяли морську свинку для дитини. Все пройшло чудово, тваринка доглянута. Дякуємо за вашу важливу працю!',
+    rate: 5,
+    author: 'Віктор Савченко',
+  },
+  {
+    _id: '667abea5e4b0c8a2a7c1c200',
+    description:
+      'Взяли папугу Кешу, він чудовий! Описи тварин на сайті повністю відповідають дійсності, що дуже важливо для майбутніх господарів.',
+    rate: 5,
+    author: 'Павло Захарченко',
+  },
+  {
+    _id: '667abea5e4b0c8a2a7c1c213',
+    description:
+      'Дякуємо! Ваша діяльність змінює світ на краще. Раді, що стали частиною цієї доброї справи.',
+    rate: 5,
+    author: 'Євгенія Лимар',
+  },
+  {
+    _id: '667abea5e4b0c8a2a7c1c217',
+    description:
+      'Нам детально розповіли про характер собаки, його звички та страхи. Це дуже допомогло у перші дні.',
+    rate: 5,
+    author: 'Інна Кузьменко',
+  },
+]);
