e3d34ae3b77e538f206234377deecc2ec6e46a6e
seroga <sergijvchichur@gmail.com>
2026-06-16 19:09:41 +0300

    test

--- src/js/pets-list.js
+++ src/js/pets-list.js
@@ -1,54 +1,16 @@
 import axios from "axios";
 
-const API_URL = axios.create({
-    baseURL: "https://paw-hut.b.goit.study/api/categories",
-});
+const API_URL = axios.create("https://paw-hut.b.goit.study/api/categories",);
 
-const getCategories = async () => {
+const getCategories = async (id, name) => {
     const response = await API_URL.get('')
     return response.data;
 }
 
-const categoryTranslations = {
-    "dogs": "Собаки",
-    "cats": "Коти",
-    "rabbits": "Кролики",
-    "rodents": "Гризуни",
-    "birds": "Птахи",
-    "special": "Тварини з особливими потребами",
-    "urgent": "Терміново шукають дім",
-};
 
-const filterBtns = document.querySelectorAll('[data-category]');
+const filterContainer = document.querySelector('.pets-list'); 
 
-filterBtns.forEach(button => {
-    button.addEventListener('click', async (e) => {
-        const englishCategory = e.target.dataset.category.toLowerCase();
-
-        const ukrainianName = categoryTranslations[englishCategory];
-
-        if (!ukrainianName) {
-            console.error(`Не знайдено перекладу для категорії: ${englishCategory}`);
-            return;
-        }
-
-        try {
-            const allCategories = await getCategories(); 
-            const foundCategory = allCategories.find(
-                item => item.name.toLowerCase() === ukrainianName.toLowerCase()
-            );
-
-            if (foundCategory) {
-                const categoryId = foundCategory._id || foundCategory.id;
-                const categoryName = foundCategory.name;
-
-                console.log({ id: categoryId, name: categoryName });
-            } else {
-                console.warn(`Категорію "${ukrainianName}" не знайдено на бекенді.`);
-            }
-
-        } catch (error) {
-            console.error("Помилка:", error);
-        }
-    });
+filterContainer.addEventListener('click', (e) => {
+    const btnElem = e.target.closest('[data-category]');
+    btnElem.classList.toggle('active');
 });
