fb1ae5c187ae141032dd09866d0005458e38132e
seroga <sergijvchichur@gmail.com>
2026-06-16 16:36:17 +0300

    modernized code for the api requests

--- src/js/pets-list.js
+++ src/js/pets-list.js
@@ -5,16 +5,31 @@ const API_URL = axios.create({
 });
 
 const getCategories = async () => {
-    const response = await API_URL.get('', {})
+    const response = await API_URL.get('')
     return response.data;
 }
 
-const filterBtns = document.querySelectorAll(['data-category']);
+const filterBtns = document.querySelectorAll('[data-category]');
 
 filterBtns.forEach(button => {
-    button.addEventListener('click',(e) => {
-        const category = e.target.dataset.filter;
+    button.addEventListener ('click', async (e) => {
+        const selectedCategory = e.target.dataset.category;
 
-        console.log(getCategories(category));
+        try {
+            const allCategories = await getCategories();
+
+            const foundCategory = allCategories.find((item) => {item.name === selectedCategory})
+
+            if (foundCategory){
+                const categoryId = foundCategory._id;
+                const categoryName = foundCategory.name;
+
+                console.log("Знайдено категорію для рендеру:", { id: categoryId, name: categoryName });
+            }else {
+                console.warn(`Категорію "${selectedCategory}" не знайдено на бекенді.`);
+            }
+        }catch (error) {
+            console.error("Помилка при отриманні або фільтрації категорій:", error);
+        }
     });
 });
