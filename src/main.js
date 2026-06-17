ebad11eea40e629ddd1ca6fecc3483d4fb27bd96
Yulia2123 <153316675+Yulia2123@users.noreply.github.com>
2026-06-17 20:55:00 +0300

    Fix main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import './js/pets-list.js';
 import './js/about-us.js';
 import './js/footer.js';
 
-
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
@@ -23,9 +22,9 @@ import 'css-star-rating/css/star-rating.css';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-const instance = basicLightbox.create(`
-  <img src="./img/photo.jpg" alt="photo">
-`);
+// const instance = basicLightbox.create(`
+//   <img src="./img/photo.jpg" alt="photo">
+// `);
 
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('.open-modal');
